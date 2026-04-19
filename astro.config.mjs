20e95229df4230eda2df122221341d0d5ce8946e
leowla <git@leowla.com>
2026-04-19 13:46:54 +1200

    refactor: optimise local fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,27 @@
 
 import cloudflare from "@astrojs/cloudflare";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://leowla.com",
+	fonts: [
+		{
+			provider: fontProviders.local(),
+			name: "Monaspace Radon",
+			cssVariable: "--font-monaspace-radon",
+			options: {
+				variants: [
+					{
+						src: ["./src/assets/fonts/monaspace-radon-var.woff2"],
+						weight: "100 900",
+						style: "normal",
+					},
+				],
+			},
+		},
+	],
 	vite: {
 		plugins: [tailwindcss()],
 	},
